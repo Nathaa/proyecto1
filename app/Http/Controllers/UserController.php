b6067cec95096625606a5e07d95219b66ae80119
Nathaa <42421207+Nathaa@users.noreply.github.com>
2018-08-21 15:14:13 -0600

    ,jkj
    hkj

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,6 +11,7 @@ class UserController extends Controller
             'maria',
             'fernando',
             'philip',
+            'mario',
         ];
 
         return view('users')->with([
